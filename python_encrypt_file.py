96fa5b3d216b2d1f3ed365db0934c5a57a14eccd
deadmorgan314 <45597006+deadmorgan314@users.noreply.github.com>
2023-11-16 18:42:43 +0300

    Add files via upload

---
+++ python_encrypt_file.py
@@ -0,0 +1,17 @@
+import os
+import base64
+from cryptography.fernet import Fernet
+
+def encrypt(file):
+      key = base64.urlsafe_b64encode(os.urandom(32))
+      cip=Fernet(key)
+      with open(file, "rb") as f:
+         data = f.read()
+      cip_text = cip.encrypt(data)
+      with open(file, "wb") as f:
+         f.write(cip_text)
+
+file = input()
+encrypt(file)
+
+
